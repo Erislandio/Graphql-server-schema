2486e9bc9e6706ab22d6d13efcf77c37a7797048
Erislandio Soares <erislandiosoares21@gmail.com>
2018-11-22 22:54:49 -0200

    Criação das queries

---
+++ queries/event.js
@@ -0,0 +1,26 @@
+const {
+    GraphQLID,
+    GraphQLNonNull
+} = require('graphql')
+
+const EventTye = require('./types/event')
+
+module.exports = {
+    type: EventType,
+    args: {
+        id:{
+            name: 'id',
+            type: new GraphQLNonNull(GraphQLID)
+        }
+    },
+
+    resolve: (root, args) => {
+        return new Promise((resolve, reject) => {
+            resolve({
+                id: 'rfjnerjieirncieqncqec',
+                name: 'Lauch party',
+                date: 'Today'
+            })
+        })
+    }
+}
